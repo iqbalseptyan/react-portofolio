ca92970b8a633d1e479f194912bbd10a5c7e4d0d
Iqbal Septyan <iqbalseptyan@gmail.com>
2021-08-13 01:16:10 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { Router } from "react-router-dom";
 import "./App.css";
 import Home from "./pages";
 
 function App() {
   return (
-    <>
+    <Router basename={process.env.PUBLIC_URL}>
       <Home />
-    </>
+    </Router>
   );
 }
 
